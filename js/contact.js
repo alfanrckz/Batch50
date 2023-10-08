38b480a564c7993aaefb284f20ee724dda2d0e1a
alfan2911 <alfansyuriziaulhaq11@gmail.com>
2023-10-08 16:51:30 +0700

    day-6

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,36 @@
+// Getting and submit data from form contact
 function submitData() {
+
     let name = document.getElementById("input-name").value;
     let email = document.getElementById("input-email").value;
-    let number = document.getElementById("input-number").value;
+    let phoneNumber = document.getElementById("input-number").value;
     let subject = document.getElementById("input-subject").value;
     let message = document.getElementById("input-message").value;
 
-    if (name === "") {
-        return alert("Name must be filled");
-    } else if (email === "") {
-        return alert("Email must be filled");
-    } else if (number === "") {
-        return alert("Number must be filled");
-    } else if (subject === "") {
-        return alert("Subject must be filled");
-    } else if (message === "") {
-        return alert("Message must be filled");
+
+    // form validation
+    if (name === "" || email === "" || phoneNumber === "" || subject === "" || message === "") {
+        return alert("Form must be filled")
+    } else if (!hasOnlyDigit(phoneNumber)) {
+        return alert("Phone number must be valid number")
+    } else if (!isValidEmail(email)) {
+        return alert("Email is not valid")
     }
 
-    let emailReceiver = "alfansyuriziaulhaq11@gmail.com";
-
+    // Create new mail in Gmail with input from user.
+    let emailReceiver = "alfansyuriziaulhaq11@gmail.com"
     let a = document.createElement("a");
-    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Halo, nama saya ${name}, ${message}. Tolong hubungi saya di nomor ${number} atau email saya di ${email}`;
+    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Halo, nama saya ${name}, ${message}. Tolong hubungi saya di nomor ${phoneNumber} atau email saya di ${email}`;
+    a.target = "_blank";
     a.click();
+}
 
-    let messagers = {
-        nama: name,
-        email: email,
-        number: number,
-        subject: subject,
-        message: message,
-    }
-    console.log(messagers);
+// Check if string contains only number or not
+function hasOnlyDigit(number) {
+    return number.match("^[0-9]+$");
+}
 
+// Email validation
+function isValidEmail(email) {
+    return email.match("^.+@[^\.].*\.[a-z]{2,}$");
 }
